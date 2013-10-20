61b9ad7557d2bbda386de075bd68f23debc1fd9a
Xiao Yuguang <xiaoyuguang24@gmail.com>
2013-10-20 18:43:30 +0800

    page layout change

--- html/logic.js
+++ html/logic.js
@@ -2,6 +2,7 @@
 	
 	$("#btnGo").click(function(){
 			$("#result").css("visibility","visible");
+			clearResult();
 			getResult();
 	});
 	
@@ -26,12 +27,34 @@ function getResult(){
 	});
 	
 }
-
+function clearResult(){
+	$("#result").html("");
+}
 function displayResult(data){
 	var urls = data.split(" ");
 	for(var i=0;i<urls.length;i++){
-		var result = "<div class='result'>";
-		result += "<img src='" + urls[i] + "'></div>";
+		
+		var result ="<div class='result'>";
+		result += "<a target='_blank' href='" + urls[i] + "'>" 
+		result += "<img src='" + urls[i] + "'></a></div>";
 		$(result).appendTo("#result");
 	}
+	adjustSize();
+}
+function adjustSize(){
+	$("#result").find("img").each(function(){
+		var imgClass = ($(this).width()/$(this).height() > 1) ? 'wide' : 'tall';
+		$(this).addClass(imgClass);
+		var parent = $(this).parents(".result").get(0);
+		if( $(parent).height() > $(this).height()){
+		    var h = $(parent).height()/2;
+			h -= $(this).height()/2;
+			$(this).css("margin-top",h+"px");
+		}
+		if($(parent).width() > $(this).width()){
+			var w = $(parent).width()/2;
+			w -= $(this).width()/2;
+			$(this).css("margin-left",w+"px");
+		}
+	});
 }
